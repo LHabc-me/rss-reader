0919680aff31c437837ae4eb209d3b7b0d8fdf2b
CJanus <cht7janus@gmail.com>
2023-09-14 04:38:32 -0700

    csv转json函数实现

--- spider/rss_spider/CSVtoJSON.py
+++ spider/rss_spider/CSVtoJSON.py
@@ -1,18 +1,33 @@
+import csv
 import json
-import pandas as pd
 
-def convert(csv_data, output_path):
-    columns = csv_data.columns.tolist()
-    output = {}
-    for col in columns:
-        output[col] = str(csv_data.loc[0, col])
-    json_data = json.dumps(output)
-    with open(output_path,'w') as jsonFile:
-        jsonFile.write(json_data)
 
+
+def convert():
+    # 读取CSV文件并转换为字典列表
+    with open('github_rss.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        headers = next(csv_reader)  # 读取头部
+
+        # 用于更改列名的映射
+        column_name_map = {
+            'Names': 'name',
+            'Links': 'link'
+        }
+
+        # 更改列名
+        headers = [column_name_map.get(header, header) for header in headers]
+
+        data_list = []
+        for row in csv_reader:
+            row_dict = {headers[i]: value for i, value in enumerate(row)}
+            data_list.append(row_dict)
+
+    # 将字典列表写入JSON文件
+    with open('github_rss.json', 'w', encoding='utf-8') as json_file:
+        json.dump(data_list, json_file, ensure_ascii=False, indent=4)
+
+    print("CSV转换为JSON完成")
 
 if __name__ == '__main__':
-   csv_path = 'github_rss.csv'
-   outputPath = 'github_rss.json'
-   csvData = pd.read_csv(csv_path, header=0)
-   convert(csvData, outputPath)
+    convert()
